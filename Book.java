9f108c78f73f1a967a63d3fc8073f8397647e8d3
Nabemh <nhanior@gmail.com>
2024-07-29 14:29:37 +0100

    book class initialized

---
+++ Book.java
@@ -0,0 +1,14 @@
+public class Book {
+    private String id, title, author, publisher, year, isbn, copies;
+
+    public Book(String id, String title, String author, String publisher, String year, String isbn, String copies) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.year = year;
+        this.isbn = isbn;
+        this.copies = copies;
+    }
+
+}
